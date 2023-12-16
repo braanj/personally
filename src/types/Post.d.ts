b3dc12c563663747c80ea769efa16b58c8583475
Brahim Anjjar <brahim@devforyou.fr>
2023-12-16 20:33:51 +0100

    set up api

---
+++ src/types/Post.d.ts
@@ -0,0 +1,14 @@
+export interface Post {
+  [x: string]: Any;
+
+  title: string;
+  description: string;
+  author: string;
+  category: string | string[];
+  country: string;
+  image?: string | null;
+  language: string | string[];
+  published_at: string;
+  source: string;
+  slug: string;
+}
